d8f1a44b787f52183dfcd3803c925ddcb2b01206
dev2 <dev2@village-justice.com>
2022-10-28 11:46:25 +0200

    install pm2 v5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host : 'ssh-tete-de-linotte.alwaysdata.net',
       ref  : 'origin/main',
       repo : 'git@github.com:RobinPoulet/express-mongoDb-oc.git',
-      path : '/home/tete-de-linotte',
+      path : '/home/tete-de-linotte/tdl_backend',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
