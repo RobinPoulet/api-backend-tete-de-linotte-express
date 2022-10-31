d25ad19b33baf2982cd64589f52f37211ec8a340
dev2 <dev2@village-justice.com>
2022-10-31 15:54:56 +0100

    addon

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use('/', () => console.log('toto'));  
   
 app.use('/api/stuff', stuffRoutes);
 
