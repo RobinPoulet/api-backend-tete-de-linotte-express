25b500579b8ad1c9cd242b84141a17e0f52b82f0
dev2 <dev2@village-justice.com>
2023-01-06 09:55:27 +0100

    add imageUrl field to category Model

--- models/category.js
+++ models/category.js
@@ -8,6 +8,10 @@ const categorySchema = mongoose.Schema({
     description: {
         type: String,
         required: true
+    },
+    imageUrl: {
+        type: String,
+        required: false
     }
 });
 
