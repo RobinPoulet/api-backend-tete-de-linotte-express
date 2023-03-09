bac67b41e04ea5e72b4243bfd634b1646f661573
Dev2 <dev2@iMac-de-Dev2.local>
2023-03-09 12:47:30 +0100

    add category seeder

---
+++ seeders/category.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Category = require('../models/category');
+const faker = require('faker');
+
+mongoose.connect('mongodb+srv://'+mongoUser+':'+mongoPassword+'@cluster0.zmhsivz.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+
+const categories = [];
+
+for (let i = 0; i < 12; i++) {
+  const category = new Category({
+    name: faker.commerce.department(),
+    description: faker.lorem.sentence(),
+  });
+  categories.push(category);
+}
+
+Category.insertMany(categories)
+  .then(() => {
+    console.log('Categories added successfully');
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.log(error);
+    mongoose.connection.close();
+  });
