53e0d1ec980bc567901233dc9979d03c0a2f9595
Dev2 <dev2@iMac-de-Dev2.local>
2023-03-09 13:14:46 +0100

    add seeders

--- seeders/category.js
+++ seeders/category.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Category = require('../models/category');
-const faker = require('faker');
+const { faker } = require('@faker-js/faker');
 
-mongoose.connect('mongodb+srv://'+mongoUser+':'+mongoPassword+'@cluster0.zmhsivz.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+const mongoUser = process.env.DB_USER;
+const mongoPassword = process.env.DB_PASSWORD;
+
+console.log(mongoUser, mongoPassword);
+
+mongoose.connect('mongodb+srv://'+mongoUser+':'+mongoPassword+'@cluster0.zmhsivz.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const categories = [];
 
